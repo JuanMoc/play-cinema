e9725ce3edc4f935ab038e2ae69c82e123ee3351
JuanMoc <hello.juan.moc@gmail.com>
2024-11-12 18:02:26 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+template = Jinja2Templates(directory="templates")
+
+
+@app.get('/')
+def index(req: Request):
+    return template.TemplateResponse(
+        name="index.html",
+        context={"request": req}
+    )
